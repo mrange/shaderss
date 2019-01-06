a98b849a0dbb04b2e88ddea435844203af37c1e3
mrange <marten_range@hotmail.com>
2019-01-06 12:47:02 +0100

    Extracted shaders

---
+++ src/shaderss/vertex_shader.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+
+char const * const get_vertex_shader ()
+{
+  return R"SHADER(
+#version 430
+
+layout (location=0) in vec2 inVer;
+out vec2 p;
+
+out gl_PerVertex
+{
+  vec4 gl_Position;
+};
+
+
+void main()
+{
+  gl_Position=vec4(inVer,0.0,1.0);
+  p=inVer;
+}
+)SHADER";
+}
