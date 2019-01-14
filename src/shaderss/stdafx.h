c3b127675ba7bb8764518067237366b095c47683
mrange <marten_range@hotmail.com>
2019-01-14 20:34:06 +0100

    PCH tweaks

--- src/shaderss/stdafx.h
+++ src/shaderss/stdafx.h
@@ -3,14 +3,31 @@
 #include "targetver.h"
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
 #include <windows.h>
 #include <windowsx.h>
 #include <wincodec.h>
+
+#include <commctrl.h>
+#include <shobjidl_core.h>
+
 #include <GL/gl.h>
 
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <algorithm>
 #include <cassert>
+#include <cstdint>
+#include <cwchar>
+#include <exception>
 #include <memory>
 #include <regex>
 #include <stdexcept>
 #include <string>
+#include <type_traits>
 #include <utility>
+#include <vector>
+
+
